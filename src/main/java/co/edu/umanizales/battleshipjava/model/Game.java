42ad6c79b6a8e070b608e77b156e1621dd8154de
Cristian <cgiraldo93759@umanizales.edu.co>
2022-05-12 11:39:55 -0500

    Creación de la clase Game

---
+++ src/main/java/co/edu/umanizales/battleshipjava/model/Game.java
@@ -0,0 +1,39 @@
+package co.edu.umanizales.battleshipjava.model;
+
+import lombok.Data;
+
+@Data
+public class Game {
+    private int id;
+    private Board player1Board;
+    private Board player2Board;
+    private byte shipsNumber;
+    private byte turn;
+    private byte p1Hits;
+    private byte p2Hits;
+
+    public Game(User player1, User player2, ListDE shipsList) {
+        createBoards(player1, player2, shipsList);
+        this.shipsNumber = (byte)shipsList.getCount();
+    }
+    public String validateShoot(int x, int y, User player) {
+        return null;
+    }
+    private void createBoards(User player1, User player2, ListDE shipsList) {
+        if(shipsList.getCount() <= 9) {
+            this.player1Board = new Board((byte)10, (byte)10, player1, shipsList);
+            this.player2Board = new Board((byte)10, (byte)10, player2, shipsList.cloneList());
+        }
+        else if(shipsList.getCount() >= 10 &&  shipsList.getCount() <= 20) {
+            this.player1Board = new Board((byte)20, (byte)20, player1, shipsList);
+            this.player2Board = new Board((byte)20, (byte)20, player2, shipsList.cloneList());
+        }
+        else {
+            this.player1Board = new Board((byte)30, (byte)30, player1, shipsList);
+            this.player2Board = new Board((byte)30, (byte)30, player2, shipsList.cloneList());
+        }
+    }
+    public User validateWinner() {
+        return null;
+    }
+}
