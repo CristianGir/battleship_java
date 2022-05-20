cf0ecae9103f4045e287cbbd4008258bfb8330e9
Miospi <miospi88@gmail.com>
2022-05-20 14:20:58 -0500

    Creación de clases

---
+++ src/main/java/co/edu/umanizales/battleshipjava/controller/ErrorHandlerConfig.java
@@ -0,0 +1,29 @@
+package co.edu.umanizales.battleshipjava.controller;
+
+import co.edu.umanizales.battleshipjava.model.GenericResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@ControllerAdvice
+public class ErrorHandlerConfig {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<?> handle(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        GenericResponse response = new GenericResponse(409, null, errors.toString());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
