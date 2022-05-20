70d558dee2c8e0b7c40e6a6d82a9a0dda07fb1c0
Miospi <miospi88@gmail.com>
2022-05-20 15:17:51 -0500

    Actualización de clases.

--- src/main/java/co/edu/umanizales/battleshipjava/controller/ListDeController.java
+++ src/main/java/co/edu/umanizales/battleshipjava/controller/ListDeController.java
@@ -1,35 +1,49 @@
 package co.edu.umanizales.battleshipjava.controller;
 
 import co.edu.umanizales.battleshipjava.Service.ListDeService;
+import co.edu.umanizales.battleshipjava.model.GenericResponse;
 import co.edu.umanizales.battleshipjava.model.Ship;
 import co.edu.umanizales.battleshipjava.model.ShipDistribution;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/listde")
+@RequestMapping(path = "/ships")
 public class ListDeController {
     @Autowired
     private ListDeService listDeService;
 
     @GetMapping
-    public List<ShipDistribution> getAllLinked()
-    {
-        return listDeService.getAllLinked();
+    public ResponseEntity<GenericResponse> getShips() {
+        return new ResponseEntity<GenericResponse>(
+                new GenericResponse(200, listDeService.getAllLinked(), null),
+                HttpStatus.OK
+        );
     }
 
     @PostMapping
-    public String createShip(@RequestBody Ship ship)
+    public ResponseEntity<GenericResponse> createShip(@RequestBody Ship ship)
     {
         ShipDistribution shipDistribution = new ShipDistribution(ship);
-        return listDeService.add(shipDistribution);
+        return new ResponseEntity<GenericResponse>(
+                new GenericResponse(200, listDeService.add(shipDistribution),
+                        null),
+                HttpStatus.OK
+        );
     }
 
     @PostMapping(path="/tostart")
-    public String createShipToStart(@RequestBody Ship ship){
+    public ResponseEntity<GenericResponse> createShipToStart(@RequestBody Ship ship){
         ShipDistribution shipDistribution = new ShipDistribution(ship);
-        return listDeService.addFirst(shipDistribution);
+        return new ResponseEntity<GenericResponse>(
+                new GenericResponse(200, listDeService.add(shipDistribution),
+                        null),
+                HttpStatus.OK
+        );
     }
 }
